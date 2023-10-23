86bba36b0576b4e7bab9999c695b1fb6429fe541
JasonCrk <emerzonkolki@gmail.com>
2023-10-23 09:15:45 -0500

    feat: firebase messaging config and initialize

---
+++ public/firebase-messaging-example.js
@@ -0,0 +1,30 @@
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/10.4.0/firebase-app-compat.js'
+// )
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging-compat.js'
+// )
+
+// const firebaseConfig = {
+//   apiKey: '',
+//   authDomain: '',
+//   projectId: '',
+//   storageBucket: '',
+//   messagingSenderId: '',
+//   appId: '',
+// }
+
+// const app = firebase.initializeApp(firebaseConfig)
+// const messaging = firebase.messaging(app)
+
+// messaging.onBackgroundMessage(payload => {
+//   const notificationTitle = payload.notification.title
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//   }
+
+//   return self.registration.showNotification(
+//     notificationTitle,
+//     notificationOptions
+//   )
+// })
---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { FirebaseOptions, initializeApp } from 'firebase/app'
+import { getMessaging } from 'firebase/messaging'
+
+import config from './config'
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: config.FIREBASE_API_KEY,
+  authDomain: config.FIREBASE_AUTH_DOMAIN,
+  projectId: config.FIREBASE_PROJECT_ID,
+  storageBucket: config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: config.FIREBASE_APP_ID,
+}
+
+const app = initializeApp(firebaseConfig)
+export const messaging = getMessaging(app)
