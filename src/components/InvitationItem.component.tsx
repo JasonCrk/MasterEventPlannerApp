dcb5532de7833e948bd00a356282d1bb9ded5f18
JasonCrk <emerzonkolki@gmail.com>
2023-11-01 21:45:28 -0500

    base del componente InvitationItem

---
+++ src/components/InvitationItem.component.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const InvitationItem: FC = () => {
+  return (
+    <div>
+      <button className='btn btn-danger'>Hello</button>
+    </div>
+  )
+}
+
+export default InvitationItem
