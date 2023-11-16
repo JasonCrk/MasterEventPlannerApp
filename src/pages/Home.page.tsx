77deeb7c26e6322f42274aee96a5c7be368f2028
Axel <axelfernandodavalossanchez@gmail.com>
2023-11-16 16:29:14 -0500

    base del componente Connection Item

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,5 +1,15 @@
+import ConnectionItem from '../components/ConnectionItem.component'
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <ConnectionItem
+        avatar='https://unavatar.io/midudev/'
+        userId='usuario123'
+        username='Usuario12'
+      />
+    </div>
+  )
 }
 
 export default Home
