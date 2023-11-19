2b070553bd1d345623e070afc8ce8b60041dfc2f
JasonCrk <emerzonkolki@gmail.com>
2023-11-19 00:26:47 -0500

    style(component): add aspect ratio to avatar component

--- src/components/Avatar.component.tsx
+++ src/components/Avatar.component.tsx
@@ -1,19 +1,16 @@
 import { FC, ImgHTMLAttributes } from 'react'
 
-import { AccountPicture } from '../models/account.model'
-
 import config from '../config'
 
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   size: string
-  src: AccountPicture | undefined
 }
 
 const Avatar: FC<Props> = ({ size, src, style, className, ...props }) => {
   return (
     <img
       className={`rounded-circle ${className}`}
-      style={{ width: size, height: size, ...style }}
+      style={{ width: size, height: size, ...style, aspectRatio: 1 / 1 }}
       src={src ?? config.DEFAULT_AVATAR_URL}
       {...props}
     />
