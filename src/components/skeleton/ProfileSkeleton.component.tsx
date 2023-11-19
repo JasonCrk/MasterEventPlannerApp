72cd3d4782697b51331f4f5560ded66c02a553ae
JasonCrk <emerzonkolki@gmail.com>
2023-11-19 00:46:48 -0500

    feat(ProfileSkeleton.component.tsx): complete skeleton

--- src/components/skeleton/ProfileSkeleton.component.tsx
+++ src/components/skeleton/ProfileSkeleton.component.tsx
@@ -1,7 +1,47 @@
 import { FC } from 'react'
 
 const ProfileSkeleton: FC = () => {
-  return <div>ProfileSkeleton</div>
+  return (
+    <section>
+      <div
+        className='position-relative bg-dark-subtle w-100'
+        style={{ height: '200px' }}
+      >
+        <div
+          className='position-absolute'
+          style={{
+            bottom: -90,
+            left: 40,
+          }}
+        >
+          <div
+            className='position-relative rounded-circle bg-secondary'
+            style={{ width: '180px', height: '180px' }}
+          ></div>
+        </div>
+      </div>
+
+      <div
+        className='container-lg px-5 position-relative'
+        style={{ paddingTop: '100px' }}
+      >
+        <p className='placeholder-glow mb-3'>
+          <span
+            className='placeholder placeholder-lg w-100'
+            style={{ height: '40px' }}
+          ></span>
+        </p>
+
+        <p className='placeholder-glow d-flex flex-wrap gap-1'>
+          <span className='placeholder col-4'></span>
+          <span className='placeholder col-7'></span>
+          <span className='placeholder col-7'></span>
+          <span className='placeholder col-4'></span>
+          <span className='placeholder col-5'></span>
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default ProfileSkeleton
